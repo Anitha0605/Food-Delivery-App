01f008b35aa546daa260d0e8656191cbfc041df7
Ani Sudhakar <anithasth92@gmail.com>
2026-04-07 18:10:05 +0530

    url update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx' // இதை இம்போர்ட் செய்யவும்
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <StoreContextProvider> {/* உங்கள் App-ஐ இப்படி உள்ளே வைக்கவும் */}
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>
 )
